b4e133c5cde0b99883de25e13eef7edcc4ea63e3
Kaspar Boel Kjeldsen <kaspar.kjeldsen@kraftvaerk.com>
2025-05-06 11:19:40 +0200

    fix up after PR

--- kjeldsen.backend/code/delivery/Delivery.cs
+++ kjeldsen.backend/code/delivery/Delivery.cs
@@ -1,7 +1,8 @@
 ﻿using kjeldsen.backend.code.delivery.Provider;
 using kjeldsen.backend.code.delivery.ResponseBuilder;
-using kjeldsen.backend.code.delivery.SchemaFilter;
+
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DeliveryApi;
@@ -15,7 +16,21 @@ public class Delivery : IComposer
     {
         builder.Services.Decorate<IApiContentResponseBuilder, CacheKeyDecoratingResponseBuilder>();
 
-        builder.Services.AddTransient<IPostConfigureOptions<SwaggerGenOptions>, RemoveDeliveryApiSchemaFilterPostConfigure>();
+        builder.Services.PostConfigure<SwaggerGenOptions>(options =>
+        {
+            options.DocumentFilter<CustomSchemaFilter>();
+        });
         builder.Services.AddSingleton<IApiMediaUrlProvider, AbsolutePathApiUrlProvider>();
+    }
+}
+
+public class CustomSchemaFilter : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        foreach ((string schemaId, OpenApiSchema schema) in context.SchemaRepository.Schemas.Where(x => x.Key.InvariantEndsWith("PropertiesModel")))
+        {
+            schema.Properties["cacheKeys"] = new OpenApiSchema() { Type = "string", Format = "uuid" };
+        }
     }
 }
